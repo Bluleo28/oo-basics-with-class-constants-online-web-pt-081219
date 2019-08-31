f37f3dda73b842635ada8a389c4cff95858df7ca
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-31 20:52:42 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
   
-    BRANDS = ["3","Uggs", "Rainbow"] 
+    BRANDS = ["Uggs", "Rainbow"] = (3)
 
   def initialize(brand)
     @all_brand = brand
